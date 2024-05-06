b515554478f7ec606b713183da6ae6058c93acc0
malloc-nbytes <zacharydhaskins@yahoo.com>
2024-05-05 20:42:58 -0500

    more updates to the interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -5,9 +5,30 @@ import qualified Data.Map as Map
 import Token
 import Ast
 
+data Ctx a
+  = Ctx
+  { ctxTbl :: [Map.Map Token a]
+  }
+
+pushScope :: Ctx a -> Ctx a
+pushScope ctx = ctx{ctxTbl = (ctxTbl ctx) ++ []}
+
+popScope :: Ctx a -> Ctx a
+popScope _ = undefined
+
+evaluateStmt :: Stmt -> Ctx a -> IO ()
+evaluateStmt stmt =
+  case stmt of
+    StmtDef sd -> undefined
+    StmtLet sl -> undefined
+    StmtBlock sb -> undefined
+    StmtMut sm -> undefined
+    StmtExpr se -> undefined
+
 interpret :: Program -> IO ()
-interpret _ = do
-  let nv = Map.empty :: Map.Map Token a
+interpret program = do
+  let namedValues = Map.empty :: Map.Map Token a
+  let context = Ctx [namedValues]
 
 
   print "EARL: Finished interpreting"
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -42,7 +42,6 @@ isIgnorable c = case c of
 lexFile :: [Char] -> String -> Int -> Int -> [Token]
 lexFile [] fp r c = [Token "EOF" TTyEof r c fp]
 lexFile ('\n':xs) fp r _ = lexFile xs fp (r+1) 1
-
 lexFile (x@'+':x'@'=':xs) fp r c = [Token ([x]++[x']) TTyPlusEquals r c fp] ++ lexFile xs fp r (c+1)
 lexFile (x@'-':x'@'=':xs) fp r c = [Token ([x]++[x']) TTyMinusEquals r c fp] ++ lexFile xs fp r (c+1)
 lexFile (x@'*':x'@'=':xs) fp r c = [Token ([x]++[x']) TTyAsteriskEquals r c fp] ++ lexFile xs fp r (c+1)
