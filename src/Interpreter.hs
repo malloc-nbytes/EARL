4873b4cccdc0eeec2405986a6cae121a1f47b8d9
malloc-nbytes <zacharydhaskins@yahoo.com>
2024-05-06 00:54:34 -0500

    added integer in interpreter

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -19,7 +19,7 @@ newtype Ctx a
   } deriving Show
 
 -- Function to print contents of ctxTbl
-printCtxTbl :: Show a => Ctx a -> IO ()
+printCtxTbl :: (Show a, Num a) => Ctx a -> IO ()
 printCtxTbl (Ctx tbl) = mapM_ printMap tbl
   where
     printMap :: Show a => Map.Map Token (Variable a) -> IO ()
@@ -28,7 +28,7 @@ printCtxTbl (Ctx tbl) = mapM_ printMap tbl
       mapM_ printVar (Map.toList mp)
 
     printVar :: Show a => (Token, Variable a) -> IO ()
-    printVar (token, var) = putStrLn $ "Token: " ++ show token ++ ", Variable: " ++ show var
+    printVar (token, var) = putStrLn $ "Token: " ++ (tokenLexeme token) ++ ", Variable: " ++ show var
 
 pushScope :: Ctx a -> Ctx a
 pushScope ctx = ctx{ctxTbl = [] ++ (ctxTbl ctx)}
@@ -58,29 +58,31 @@ addVarToScope token varType eval (Ctx tbl) = Ctx $ case tbl of
   where
     var = Variable token varType eval
 
-evaluateExpr :: Expr -> Ctx a -> a
+evaluateExpr :: Num a => Expr -> Ctx a -> a
 evaluateExpr expr ctx =
   case expr of
     ExprTerm (TermIdent tok) ->
       case getVarFromScope tok ctx of
         Just var -> variableEval var
         Nothing -> err ErrorUndeclared ("variable " ++ tokenLexeme tok ++ " is not defined") $ Just tok
-    ExprTerm (TermIntlit tok) -> undefined
+    ExprTerm (TermIntlit tok) ->
+      let intVal = read (tokenLexeme tok) :: Int -- Extract integer value from token
+      in fromIntegral intVal -- Convert to the appropriate type if needed and return
     ExprTerm (TermStrlit tok) -> undefined
     ExprBinary (left, op, right) -> undefined
     ExprFuncCall funcId -> undefined
     _ -> error "evaluateExpr: invalid expression"
 
-evaluateLetStmt :: LetStmt -> Ctx a -> Ctx a
+evaluateLetStmt :: Num a => LetStmt -> Ctx a -> Ctx a
 evaluateLetStmt (LetStmt elsId elsTy elsExpr) ctx =
   if isVarInScope elsId ctx then
-    let msg = "variable " ++ tokenLexeme elsId ++ "is already defined"
+    let msg = "variable " ++ tokenLexeme elsId ++ " is already defined"
     in err ErrorRedeclared msg $ Just elsId
   else
     let expr = evaluateExpr elsExpr ctx
     in addVarToScope elsId elsTy expr ctx
 
-evaluateStmt :: Stmt -> Ctx a -> IO (Ctx a)
+evaluateStmt :: Num a => Stmt -> Ctx a -> IO (Ctx a)
 evaluateStmt stmt ctx = do
   case stmt of
     StmtDef _ -> error "StmtDef: unimplemented"
@@ -90,13 +92,13 @@ evaluateStmt stmt ctx = do
     StmtExpr _ -> error "StmtExpr: unimplemented"
     _ -> error "evaluateStmt: invalid statement"
 
-evaluateStmts :: [Stmt] -> Ctx a -> IO (Ctx a)
+evaluateStmts :: Num a => [Stmt] -> Ctx a -> IO (Ctx a)
 evaluateStmts [] ctx = return ctx
 evaluateStmts (x:xs) ctx = do
   ctx' <- evaluateStmt x ctx
   evaluateStmts xs ctx'
 
-interpret :: Program -> IO (Ctx ())
+interpret :: (Num a, Show a) => Program -> IO (Ctx a)
 interpret program = do
   ctx <- evaluateStmts (extractStmts program) (Ctx [Map.empty])
   printCtxTbl ctx
