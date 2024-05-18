5523b23532a9303c4c38af2fd7ba8253ab9fec4d
malloc-nbytes <zdhdev@yahoo.com>
2024-05-18 00:01:27 -0500

    added colors to errors

--- src/test/test-suite.c
+++ src/test/test-suite.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "test-suite.h"
 #include "vec/test-vec.h"
 #include "pair/test-pair.h"
 
-int main(void) {
-  int pass = 0, fail = 0;
+int
+main(void) {
+  int pass = 0;
+  int fail = 0;
 
+  time_t start, end;
+  double elapsed;
+
+  start = time(NULL);
   RUN_TEST(test_vector_instantiation());
   RUN_TEST(test_vector_insert_elems());
   RUN_TEST(test_vector_large_elements());
@@ -15,7 +22,11 @@ int main(void) {
 
   RUN_TEST(test_pair_instantiation());
   RUN_TEST(test_pair_same_types_ints());
+  end = time(NULL);
 
+  elapsed = difftime(end, start);
+
+  printf("[test] total time: %.5f\n", elapsed);
   printf("TEST SUMMARY\n");
   printf("  PASS: %-5d FAIL: %d\n", pass, fail);
 
