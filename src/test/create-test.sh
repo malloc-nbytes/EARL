9a75baea67f8eb6df8a7aa79bb6b51c833c100a5
malloc-nbytes <zdhdev@yahoo.com>
2024-05-19 16:51:04 -0500

    added printing in the test script

--- src/test/create-test.sh
+++ src/test/create-test.sh
@@ -19,11 +19,17 @@ fi
 TESTNAME=$1
 INCLUDE_DIRECTIVE="#include \"$TESTNAME/test-$TESTNAME.h\""
 
-sed -i "s/test_suite_SOURCES = \(.*\)/test_suite_SOURCES = \1 ${TESTNAME}\/test-${TESTNAME}.c/" Makefile.am
+echo "TESTNAME: $TESTNAME"
+echo "INCLUDE_DIRECTIVE: $INCLUDE_DIRECTIVE"
 
-mkdir "$TESTNAME"
+sed -i "s/test_suite_SOURCES = \(.*\)/test_suite_SOURCES = \1 ${TESTNAME}\/test-${TESTNAME}.c/" Makefile.am
+echo "Update Makefile.am"
+
+mkdir -v "$TESTNAME"
 
 touch "$TESTNAME/test-$TESTNAME"{.c,.h}
+
+echo "Created $TESTNAME/test-$TESTNAME"{.c,.h}
 
 cat > "${TESTNAME}/test-${TESTNAME}.h" <<EOF
 #ifndef ${TESTNAME^^}_H
@@ -36,6 +42,13 @@ cat > "${TESTNAME}/test-${TESTNAME}.h" <<EOF
 #endif // ${TESTNAME^^}_H
 EOF
 
+echo "Created prototype at ${TESTNAME}/test-${TESTNAME}.h"
+
 echo -e "#include \"test-suite.h\"\n\n// Add test implementations here" > "$TESTNAME/test-$TESTNAME.c"
 
+echo "Created implementation file at $TESTNAME/test-$TESTNAME.c"
+
 sed -i "1i$INCLUDE_DIRECTIVE" test-suite.c
+
+echo "Added necessary #include to test-suite.c"
+echo "Done"
