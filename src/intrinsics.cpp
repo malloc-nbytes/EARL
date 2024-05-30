b5e1eecfdf741e7aeffbb012afc1d4be1efa5833
malloc-nbytes <zdhdev@yahoo.com>
2024-05-29 20:07:11 -0500

    printing now works again

--- src/intrinsics.cpp
+++ src/intrinsics.cpp
@@ -13,10 +13,12 @@ const std::vector<std::string> Intrinsics::intrinsic_funcs = {
 };
 
 void Intrinsics::print(Ctx &ctx, ExprFuncCall *expr) {
-  for (std::unique_ptr<Expr> &e : expr->m_params) {
+  for (size_t i = 0; i < expr->m_params.size(); ++i) {
+    std::unique_ptr<Expr> &e = expr->m_params[i];
     Interpreter::ExprEvalResult param = Interpreter::eval_expr(e.get(), ctx);
     if (param.m_expr_term_type == ExprTermType::Int_Literal) {
-      std::cout << std::any_cast<int>(param.m_expr_value) << '\n';
+      std::cout << std::any_cast<int>(param.m_expr_value) <<
+        (i == expr->m_params.size()-1 ? "" : " ");
     }
     else if (param.m_expr_term_type == ExprTermType::Str_Literal) {
       assert(false && "unimplemented");
@@ -25,14 +27,14 @@ void Intrinsics::print(Ctx &ctx, ExprFuncCall *expr) {
       // Identifier
       Token *tok = std::any_cast<Token *>(param.m_expr_value);
       EarlVar &var = ctx.get_earlvar_from_scope(tok->lexeme());
-      // std::cout << var.m_value.type().name() << std::endl;
       switch (var.m_type) {
       case EarlTy::Type::Int: {
-        std::cout << std::any_cast<int>(var.m_value) << std::endl;
+        std::cout << std::any_cast<int>(var.m_value) << (i == expr->m_params.size()-1 ? "" : " ");
       } break;
       default:
         ERR_WARGS(ErrType::Fatal, "invalid type for printing (%d)", (int)var.m_type);
       }
     }
   }
+  std::cout << '\n';
 }
