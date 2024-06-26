89056195aef822ade85784f7ff257eb62a95c66c
malloc-nbytes <zdhdev@yahoo.com>
2024-06-26 14:54:25 -0500

    removed debug information

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,6 @@ int main(int argc, char **argv) {
     std::string comment = "#";
 
     std::unique_ptr<Lexer> lexer = lex_file(filepath, keywords, types, comment);
-    lexer->dump();
     std::unique_ptr<Program> program = Parser::parse_program(*lexer.get());
     Interpreter::interpret(std::move(program), std::move(lexer));
 
