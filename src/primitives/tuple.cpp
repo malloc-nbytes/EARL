78f1c3178a0f3c086b9a19b20bf620ade67dd36b
malloc-nbytes <zdhdev@yahoo.com>
2024-08-18 10:43:24 -0500

    fixed nth bug where the interpreter would not catch out of range checks

--- src/primitives/tuple.cpp
+++ src/primitives/tuple.cpp
@@ -44,7 +44,7 @@ Tuple::nth(std::shared_ptr<Obj> &idx) {
     switch (idx->type()) {
     case Type::Int: {
         auto index = dynamic_cast<Int *>(idx.get());
-        if (index->value() < 0 || static_cast<size_t>(index->value()) > this->value().size()) {
+        if (index->value() < 0 || static_cast<size_t>(index->value()) >= this->value().size()) {
             std::string msg = "index "+std::to_string(index->value())+" is out of range of length "+std::to_string(this->value().size());
             throw InterpreterException(msg);
         }
